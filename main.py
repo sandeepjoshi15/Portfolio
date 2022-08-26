53ed03a55889dd69e90c3b1ec4ee025882e06cdc
sandeepjoshi15 <37767739+sandeepjoshi15@users.noreply.github.com>
2022-08-26 13:39:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,15 +17,15 @@ col1, col2 = st.columns([4, 1])
 
 with col1:
     st.markdown("<h2 style='text-align: center; color: Grey;'>Hi! I am</h2>", unsafe_allow_html=True)
-    st.markdown("<b><p style='font-family:Cursive;font-size:400%;text-align: center; color: Black;'>Sandeep Joshi</p></b>", unsafe_allow_html=True)
+    st.markdown("<b><p style='font-family:Cursive;font-size:400%;text-align: center; color: Black;text-shadow: 0 0 10px #ccffcc, 0 0 20px #ccffd9, 0 0 30px #ccffe6, 0 0 40px #ccfff2;'>Sandeep Joshi</p></b>", unsafe_allow_html=True)
     st.caption("<p style='text-align:center;color: Grey;'>Inquisitive and motivated individual, agog to learn, experience and create something new. I have found that nothing satisfies me more than meeting new people, developing new relationships, solving problems and contributing to the overall growth of a business. Sports and travel enthusiast who loves to play, partake in outdoor activities and find new adventures along the way.</p>", unsafe_allow_html=True)
     st.markdown("<b><p style='font-family:Cursive;font-size:200%;text-align: center; color: #262626;'>Work Experience</p></b>", unsafe_allow_html=True)
     st.markdown("<b><p style='font-family:Sans Serif;font-size:140%; color: Grey;'>ZS Associates</p></b>", unsafe_allow_html=True)
     st.caption("<p color: Grey;'>I have been working in ZS Associate since July 2021. During this period, I had learned a lot through various projects. Projects I had worked on are listed below: </p><p style='font-family:Sans Serif;font-size:100%; color: Grey;'><b>Incentive Compensation Design Project</p></b>", unsafe_allow_html=True)
-    st.caption("<h6 color: Grey;'>oAssessed the program’s alignment with strategic objectives through stakeholder interviews and sales force surveys. <br>o Analyzed the program for fairness, pay for performance, biases and fiscal responsibility<br>o Benchmarked the design and performance against key industry standards (e.g. plan type, metrics, leverage, mix)<br>o Synthesized results to create actionable insights to enhance sales compensation program performance</p>", unsafe_allow_html=True)
+    st.caption("<h6 color: Grey;'>o Assessed the program’s alignment with strategic objectives through stakeholder interviews and sales force surveys. <br>o Analyzed the program for fairness, pay for performance, biases and fiscal responsibility<br>o Benchmarked the design and performance against key industry standards (e.g. plan type, metrics, leverage, mix)<br>o Synthesized results to create actionable insights to enhance sales compensation program performance</p>", unsafe_allow_html=True)
 
 with st.expander("My Skills"):    
-    st.image(img1,width = 800)
+    st.image(img1,use_column_width = 'always')
     
 with st.expander("Certification"):
     cola, colb, colc = st.columns(3)
@@ -51,4 +51,4 @@ with st.expander("Certification"):
     colc.image("images/powerBI.PNG")
     
 with col2:
-    st.image(image)
+    st.image(image, use_column_width = 'always')
